0341ea9ea41848e9ae54db9570bb5be050d4135d
przempol <przempol777@gmail.com>
2020-01-22 15:24:54 +0100

    Added Graph exceptions and updatet docstrings

--- graph.py
+++ graph.py
@@ -2,13 +2,50 @@ import numpy as np
 import time
 
 
+class GraphError(Exception):
+    pass
+
+
+class WrongInitialValue(GraphError):
+    pass
+
+
+class WrongAlphaValue(GraphError):
+    pass
+
+
+class WrongGraphSize(GraphError):
+    pass
+
+
 class Graph:
     """
-    Class used to implement modified Barbasi Albert Graph.
+    Class used to implement modified Barbasi Albert Graph. It contains values:
+
+    int m0 - initial size of complete graph
+
+    float alpha - float from 0 to 1, in particular if 1 it is equal to standard Barbasi Albert Graph,
+    if 0 then it is random graph
+
+    int tick - current time
+
+    list nodes - list of where are nodes currently connected - it is a much faster way to store state of graph,
+    than the matrix of adjacency
+
+    list vertex_degrees - list of currently vertex degrees
+
+    int size - size of graph (number of vertexes)
 
 
+    And following methods are implemented
+
+    Graph.single_update() - update state of Graph by adding one node
+
+    Graph.update_graph_to_size(size) - update state of Graph by to desired size
+
+    Graph.test_speed(size) - measure time to create graph of desired size, without changing current state
     """
-    def __init__(self, m0: int = 3, alpha: float = 1):
+    def __init__(self, m0: int = 3, alpha: float = 1.):
         """
         Create instance of Barabasi-Albert Graph with given starting size 'm0', and custom parameter 'alpha'
         what indices probability of using PAR (preferential attachment rule - see Barabasi-Albert graphs) over random
@@ -19,8 +56,8 @@ class Graph:
         if 1 => will only use PAR (standard BA graph), if 0 => never use PAR (random graphs)
 
         """
-        self.__m0: int = m0
-        self.__alpha: float = alpha
+        self.m0 = m0
+        self.alpha = alpha
         self.__tick: int = 0
         self.__nodes: list = np.concatenate([np.full(m0 - 1, ii) for ii in range(m0)]).tolist()
 
@@ -28,9 +65,29 @@ class Graph:
     def m0(self):
         return self.__m0
 
+    @m0.setter
+    def m0(self, m0):
+        if isinstance(m0, int):
+            if m0 > 1:
+                self.__m0: int = m0
+            else:
+                raise WrongInitialValue('m0 must be integer larger than 1')
+        else:
+            raise WrongInitialValue('m0 must be integer larger than 1')
+
     @property
     def alpha(self):
         return self.__alpha
+
+    @alpha.setter
+    def alpha(self, alpha):
+        if isinstance(alpha, (float, int)):
+            if 0 <= alpha <= 1:
+                self.__alpha: float = float(alpha)
+            else:
+                raise WrongAlphaValue('alpha must be a number from 0 to 1')
+        else:
+            raise WrongAlphaValue('alpha must be a number from 0 to 1')
 
     @property
     def tick(self):
@@ -40,25 +97,33 @@ class Graph:
     def nodes(self):
         return self.__nodes
 
-    @tick.setter
-    def tick(self, tick):
-        if not isinstance(tick, int):
-            print("Not number value!")
-        else:
-            self.__tick = tick
+    @property
+    def vertex_degrees(self):
+        unique, degree_distribution = np.unique(self.nodes, return_counts=True)
+        return degree_distribution.tolist()
 
-    def update(self, desired_size: int):
+    @property
+    def size(self):
+        return self.m0 + self.tick
+
+    def update_graph_to_size(self, desired_size: int):
         """
         Update graph to desired size (number of nodes), what must obviously must be larger than current size
         While updating, it is only using graphs.append() function
 
         :param desired_size: integer (that must be larger than actual size of graph) of desired size
         """
-        repeats = desired_size - self.m0 - self.tick
-        for i in range(repeats):
-            self.append()
+        if isinstance(desired_size, int):
+            repeats = desired_size - self.size
+            if repeats <= 0:
+                raise WrongGraphSize('desired size of graph must be integer larger than current size')
+        else:
+            raise WrongGraphSize('desired size of graph must be integer larger than current size')
 
-    def append(self):
+        for i in range(repeats):
+            self.single_update()
+
+    def single_update(self):
         """
         Append just one node to the current graph. While it is modified Barabasi-Albert graphs, it compare 'alpha' value
         with random float number from 0 to 1 generated from uniform distribution. If 'alpha' is larger than random
@@ -67,6 +132,7 @@ class Graph:
         # x is current number of adding node
         x = self.tick + self.m0
         self.__nodes.append(x)
+        # rnd is number from 0 to 1, so if alpha = 1, then its always bigger than rnd
         rnd = np.random.rand()
         if self.alpha > rnd:
             size = self.m0 * (self.m0 - 1) + 2 * self.tick
@@ -77,26 +143,30 @@ class Graph:
             y = np.random.randint(x)
 
         self.__nodes.append(y)
-        self.tick += 1
+        self.__tick += 1
 
-    def test_speed(self, desired_size) -> float:
+    def test_speed(self, desired_size: int) -> float:
         """
-        Function that compute and return time (in seconds) needed to create graph of desired size, with initial values
+        Function that measure and return time (in seconds) needed to create graph of desired size, with initial values
         the same as current graph. For comparision, using matrix methods implemented in mathematica, it took about
         30 minutes to make graph of 1e4 nodes, while using list methods implemented in python, it took about 1 second
-        to make graph of 1e5 nodes
+        to make graph of 1e5 nodes.
 
         :param desired_size: integer (that must be larger than actual size of graph) of desired size
         :return time:
         """
+        if isinstance(desired_size, int):
+            if desired_size <= 0:
+                raise WrongGraphSize
+        else:
+            raise WrongGraphSize
+
         start = time.time()
-
         tmp_graph = Graph(self.m0, self.alpha)
-        repeats = desired_size - tmp_graph.m0 - tmp_graph.tick
 
+        repeats = desired_size - tmp_graph.size
         for i in range(repeats):
-            tmp_graph.append()
+            tmp_graph.single_update()
 
         end = time.time()
-        print(f'Creating modified BA graph of size {desired_size} lasted {end - start} second')
         return end - start
--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from graph import Graph
 from gui import chart
 
 if __name__ == "__main__":
-    x = Graph(m0=3, alpha=0.)
-    x.update(int(2e5))
+    g = Graph(m0=2, alpha=1)
+    g.update_graph_to_size(int(1.6e5))
+    g.single_update()
+    desired_size = int(1.6e5)
+    time = g.test_speed(desired_size)
+    print(f'Creating modified BA graph of size {desired_size} lasted {time} second')
 
-    x.test_speed(int(1e5))
-    chart(x)
+    # print(f'done {g.alpha}, zas rozmiar to {g.size}')
+    # print(g.vertex_degrees)
+    # chart(g)
 
 
